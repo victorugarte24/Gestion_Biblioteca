ee0c61b76f53820c8d2ea109a5f6474dcb14dec0
victorugarte24 <victorugarte24@opendeusto.es>
2020-05-12 12:55:04 +0200

    Limpieza POM + Corrección de forma + Actualizaciones de versiones

--- src/main/java/es/deusto/spq/main/Main.java
+++ src/main/java/es/deusto/spq/main/Main.java
@@ -2,7 +2,7 @@ package es.deusto.spq.main;
 
 import es.deusto.spq.ventanas.VentanaLogin;
 
-public class main {
+public class Main {
 	public static void main (String [ ] args) {
 		VentanaLogin v = new VentanaLogin();
 		v.setVisible(true);
